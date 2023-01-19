6c232707113aeae26b222997d1398d43f9fd9cf8
jacquelineleone <jacqueline.leone02@gmail.com>
2023-01-19 15:49:59 -0300

    avances

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './styles.css';
 import Top from "../Top/Top";
+import Footer from "../Footer/Footer";
 
 //Assets
 import amarillo from "../../assets/amarillo.png";
@@ -11,6 +12,10 @@ import item2 from "../../assets/item2.png";
 import item3 from "../../assets/item3.png";
 import grey from "../../assets/grey.png";
 import nosotras from "../../assets/nosotras.png";
+import grey2 from "../../assets/grupoDelSudAbout.png";
+import logoGrupo from "../../assets/logoGrupoDelSud.png";
+import recorte from "../../assets/recorteSobreNosotras.png";
+import backgroundPhone from "../../assets/backgroundPhone.png";
 
 export default function About() {
     return (
@@ -64,6 +69,40 @@ export default function About() {
                 <img src={nosotras}/>
                 <p className="valores-hashtag-text">#PoryPara<b className="valores-hashtag-text-highlight">Mujeres</b></p>
                 <img src={grey} className="valores-hashtag-grey"/>
+            </div>
+            <div className="about-grupo-company">
+                <div className="about-grupo-company-textContainer">
+                    <p className="about-grupo-company-text"><b className="about-grupo-company-text-highlight">Eugénie</b> es una empresa de <b className="about-grupo-company-text-highlight">Grupo Delsud</b></p>
+                    <p className="about-grupo-company-text"><b className="about-grupo-company-text-highlight">Grupo Delsud</b> es un conjunto de empresas nacionales que apuesta por el desarrollo 
+                    y la innovación de manera constante. Contamos con experiencia en distintas áreas 
+                    que nos permite seguir expandiéndonos de manera sólida y ligera.</p>
+                    <p className="about-grupo-company-text">Para conocer más ingresá en:</p>
+                </div>
+                <div className="grupo-company-button-moreInfo">
+                    <a href="https://grupodelsud.com/" className="grupo-company-linkL-moreInfo"><p className="grupo-company-link-moreInfo">grupodelsud.com</p></a>
+                </div>
+                <img src={logoGrupo} className="about-grupo-company-logo"/>
+                <img src={grey2}/>
+            </div>
+            <div className="about-recorte-cv">
+                <p className="about-recorte-cv-text">
+                    Si te interesa la propuesta y querés trabajar<br/> 
+                    con nosotras, cargá aquí tu Curriculum Vitae.
+                </p>
+                <div className="about-recorte-cv-button">
+                    <p className="about-recorte-cv-button-text">Cargar CV</p>
+                </div>
+                <img src={recorte}/>
+            </div>
+            <div className="about-recorte-wsp">
+                <p className="about-recorte-wsp-text">Si tenés alguna consulta</p>
+                <div className="about-recorte-wsp-button">
+                    <p className="about-recorte-wsp-button-text">Hablemos por Whatsapp</p>
+                </div>
+                <img src={backgroundPhone}/>
+            </div>
+            <div className="about-footer">
+                <Footer />
             </div>
         </div>
     )
--- src/components/Capacitaciones/Capacitaciones.js
+++ src/components/Capacitaciones/Capacitaciones.js
@@ -2,11 +2,31 @@ import React from "react";
 import './styles.css';
 import Top from "../Top/Top";
 
+//Assets
+import computer from '../../assets/computer.png';
+import searchIcon from '../../assets/Icon/searchIcon.png';
+
 export default function Capacitaciones() {
     return (
         <div className="capacitaciones-container">
             <Top />
-            <text>Capacitaciones</text>
+            <div className="capacitaciones-header-container">
+                <div className="capacitaciones-informacion-general">
+                    <p className="capacitaciones-informacion-general-title">CAPACITACIONES</p>
+                    <img src={computer} className="capacitaciones-informacion-general-img"/>
+                </div>
+                <div className="capacitaciones-header-explicacion">
+                    <div className="capacitaciones-header-explicacion-pregunta">
+                        <p className="capacitaciones-header-explicacion-pregunta-text">¿Por qué brindamos capacitaciones?</p>
+                        <img src={searchIcon} className="capacitaciones-header-explicacion-pregunta-searchIcon"/>
+                    </div>
+                    <div className="capacitaciones-header-explicacion-respuesta">
+                        <p className="capacitaciones-header-explicacion-respuesta-text">
+                            Creemos que capacitar es brindar las herramientas necesarias <b className="capacitaciones-header-explicacion-respuesta-highlight">para que cada mujer pueda</b> tomar sus propias decisiones, <b className="capacitaciones-header-explicacion-respuesta-highlight">manejar sus finanzas</b> de forma eficiente, <b className="capacitaciones-header-explicacion-respuesta-highlight">contribuyendo al desarrollo de sus metas<br/>y al liderazgo de sus vidas.</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
